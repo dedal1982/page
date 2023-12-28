d23422efb85b0a54f51a86737e33a44a35a4398f
aldm <d44.a1@yandex.ru>
2023-12-28 23:16:41 +0300

    wa

--- scripts/swipper.js
+++ scripts/swipper.js
@@ -57,3 +57,25 @@ const swiper = new Swiper(".bought-out-swiper", {
     el: ".swiper-scrollbar",
   },
 });
+
+const swiperReviews = new Swiper(".reviews-swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
