6c7e5b2d4552de6bd8be32c641d7df653abfbdc1
aldm <d44.a1@yandex.ru>
2023-12-28 08:13:55 +0300

    sc

--- scripts/swipper.js
+++ scripts/swipper.js
@@ -14,7 +14,28 @@ const swiper = new Swiper(".bought-out-swiper", {
     nextEl: ".bought-out-next",
     prevEl: ".bought-out-prev",
   },
-
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    375: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1920: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
   // And if we need scrollbar
   scrollbar: {
     el: ".swiper-scrollbar",
