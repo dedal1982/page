9185ea6f60446ae78c35d7c92d549c17a3c7ebef
aldm <d44.a1@yandex.ru>
2023-12-29 09:15:20 +0300

    dw

--- scripts/swipper.js
+++ scripts/swipper.js
@@ -170,3 +170,51 @@ const swiperAny = new Swiper(".any-swiper", {
     el: ".swiper-scrollbar",
   },
 });
+
+const swiperInspection = new Swiper(".inspection-swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    425: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    650: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    769: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    890: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
