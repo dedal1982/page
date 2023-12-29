129094ecb07d0969ba480a12b8ff70e2ff6d1269
aldm <d44.a1@yandex.ru>
2023-12-29 20:39:15 +0300

    wda

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,14 @@
 const burgerClick = document.querySelector(".header__burger");
 const scrollLock = document.querySelector(".page");
 const mobileMenu = document.querySelector(".burger-mobile-menu");
-
-/***открытие бургера***/
+//открытие бургера
 burgerClick.addEventListener("click", () => {
   burgerClick.classList.toggle("active");
   mobileMenu.classList.toggle("active");
   scrollLock.classList.toggle("lock");
 });
 
-/***добавляем поле для загрузки фото***/
+//добавляем поле для загрузки фото
 function addForm() {
   // Создаем новую строку формы
   var formRow = document.createElement("div");
@@ -56,3 +55,52 @@ function loadYouTubeVideo() {
     //фрейм ютюб-видео(сюда вставить iframe нужного ролика)
     '<iframe src="https://www.youtube.com/embed/oLY1rss9Jx8?si=-5eVgEZEd3nRIRP0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
 }
+
+//аккордеон
+// Получаем все элементы с классом accord-title
+// Получаем все элементы с классом "accord-title"
+var accordTitles = document.getElementsByClassName("accord-title");
+
+// Проходим по всем элементам "accord-title" и добавляем обработчик события клика
+for (var i = 0; i < accordTitles.length; i++) {
+  accordTitles[i].addEventListener("click", function () {
+    // При клике на элемент "accord-title" добавляем/удаляем класс "active"
+    this.classList.toggle("active");
+
+    // Получаем элемент "accord-answer", который находится в том же родительском элементе
+    var accordAnswer = this.nextElementSibling;
+
+    // При клике на элемент "accord-title" также добавляем/удаляем класс "active" для "accord-answer"
+    accordAnswer.classList.toggle("active");
+  });
+}
+
+// const makeCodeUniversal = (
+//   tabItemsQuery,
+//   formItemsQuery,
+//   tabClassName = "active"
+// ) => {
+//   const tabItems = Array.from(document.querySelectorAll(tabItemsQuery));
+//   const formItems = Array.from(document.querySelectorAll(formItemsQuery));
+
+//   const clearActiveTabs = (element) => {
+//     element.find((item) => item.classList.remove(tabClassName));
+//   };
+
+//   const setActiveTab = (element, index) => {
+//     element[index].classList.add(tabClassName);
+//   };
+
+//   const chekTab = (item, index) => {
+//     item.addEventListener("click", () => {
+//       clearActiveTabs(tabItems);
+//       clearActiveTabs(formItems);
+
+//       setActiveTab(tabItems, index);
+//       setActiveTab(formItems, index);
+//     });
+//   };
+//   tabItems.forEach(chekTab);
+// };
+// // /*табы / секция Наши реализованные проекты*/
+// makeCodeUniversal(".accord-title", ".accord-answer");
