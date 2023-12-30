1094f99c12167f3584a0eed9b6d820b5502f58ca
aldm <d44.a1@yandex.ru>
2023-12-30 03:40:31 +0300

    dwa

--- scripts/index.js
+++ scripts/index.js
@@ -48,14 +48,6 @@ function removeForm(event) {
 var plusButton = document.querySelector(".plusButton");
 plusButton.addEventListener("click", addForm);
 
-//функция загрузки видеоролика только при клике на кнопку
-function loadYouTubeVideo() {
-  var youtubeVideoDiv = document.getElementById("youtubeVideo");
-  youtubeVideoDiv.innerHTML =
-    //фрейм ютюб-видео(сюда вставить iframe нужного ролика)
-    '<iframe src="https://www.youtube.com/embed/oLY1rss9Jx8?si=-5eVgEZEd3nRIRP0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
-}
-
 //аккордеон
 const buttons = document.querySelectorAll(".accord-title");
 
